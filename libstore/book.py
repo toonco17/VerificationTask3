1eaa50a55220a73a1ef3f251092412e6f137fb78
ilina.diu <ilina.diu@ispras.ru>
2024-06-19 21:02:21 +0300

    looks like the cov finally works, fuck it

--- libstore/book.py
+++ libstore/book.py
@@ -1,6 +1,20 @@
 class Book:
-  def __init__(self):
-    pass
+  def __init__(self,
+               book_id = 0,
+               author = None,
+               title = "Undefined",
+               year = None,
+               price = None,
+               publisher = None,
+               genre = None):
+
+    self.book_id = book_id
+    self.author = author
+    self.title = title
+    self.year = year
+    self.price = price
+    self.publisher = publisher
+    self.genre = genre
 
   def checkParamTypes(self):
     pass
