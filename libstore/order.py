2e05db1331c8d06adfa95fd2316a819de63d0fec
ilina.diu <ilina.diu@ispras.ru>
2024-06-21 00:19:11 +0300

    Applying Order class...

--- libstore/order.py
+++ libstore/order.py
@@ -5,18 +5,21 @@ class Order:
                order_id = 0,
                client_id = 0,
                books = dict(),
-               order_status = "awaiting confirmation"):
+               order_status = 0):
 
     self.order_id = order_id
     self.client_id = client_id
     self.books = books
     self.order_status = order_status
+    
+    self.possible_order_statuses = {0 : "Awaiting confirmation", 1 : "Delivering", 2 : "Delivered", 3 : "Declined"}
 
   def checkOrderStatus(self):
-    pass
+    print(self.possible_order_statuses[self.order_status])
 
   def changeOrderStatus(self, newStatus = 0):
-    pass
+    self.order_status = newStatus
 
   def showBooksInThisOrder(self, fullInfo = False):
-    pass
+    for book_id in self.books:
+      self.books[book_id].showInfo(fullInfo = fullInfo)
