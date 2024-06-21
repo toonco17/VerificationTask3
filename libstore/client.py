ecca66feca4adca9ed05dee5629db0b32f83dd14
ilina.diu <ilina.diu@ispras.ru>
2024-06-21 16:10:53 +0300

    Basic client class functionality fixes

--- libstore/client.py
+++ libstore/client.py
@@ -16,11 +16,11 @@ class Client:
     self.basket = basket
     self.orders = orders
 
-  if (type(self.client_id) != int or self.client_id < 0):
-    raise ValueError("client_id must be non-negative int")
+    if (type(self.client_id) != int or self.client_id < 0):
+      raise ValueError("client_id must be non-negative int")
 
-  if (type(self.client_password) != str):
-    raise typeError("Client pass must be a string")
+    if (type(self.client_password) != str):
+      raise typeError("Client pass must be a string")
 
   def addBooksToBasket(self, book):
     self.basket[book.book_id] = book
